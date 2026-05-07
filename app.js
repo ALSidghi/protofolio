32f486e00ef35a886fb9e06e20ebcb79ca11bfe3
ALSidghi <ALSidghi@gmail.com>
2026-05-07 18:26:17 +0300

    ADD CV BTN

--- app.js
+++ app.js
@@ -2,13 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("Portfolio Loaded 🚀");
 });
 
-
 function showMessage() {
-
     const message = document.getElementById("message");
-
     message.innerHTML = "Thanks for visiting my portfolio 🚀";
-
     message.style.opacity = "0";
     message.style.transform = "translateY(10px)";
     message.style.textShadow = "0 0 10px #a855f7";
